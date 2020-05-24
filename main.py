bc536c3726126cc477c8b1970c7e38289d00e718
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-05-24 00:06:36 +0000

    usage

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-import pickle, time
-
 from generateData import *
 from player import *
 from simulator import *
 from team import *
 
 
-def saveData(obj, filename):
-	with open(filename, 'wb') as output:  # Overwrites any existing file.
-		pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+# USAGE
 
-def loadData(filename):
-	with open(filename, 'rb') as data:
-		return pickle.load(data)
+# create 2 teams and save
+team1 = generateTeam("Dodgers", 2018)
+team2 = generateTeam("Red Sox", 2018)
 
-times = []
-
-for i in range(50):
-	start = time.time()
-	x = generatePlayer("pujols", 2010)
-	times.append(time.time() - start)
-
-print(sum(times) / len(times))
+# create a game
+worldSeries = Game(team1, team2)
+# simulate the game
+worldSeries.simulate()
+# view the results
+print(worldSeries)
